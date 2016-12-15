746644b5999f8d3cd0501c7a6d6cd175e68e3026
ianjorgensen <jorgensen.ian@gmail.com>
2016-12-15 10:10:12 +0100

    express port

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express')
 var app = express()
 var path = require('path')
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
@@ -16,6 +18,6 @@ app.use(function (req, res, next) {
   res.status(404).send('Sorry cant find that!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
